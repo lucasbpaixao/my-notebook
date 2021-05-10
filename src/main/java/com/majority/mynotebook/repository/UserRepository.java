3540b2552f86d5508a07f18b09a3726d48096289
Lucas Paixão <lucasbpaixao@outlook.com>
2021-05-09 21:34:34 -0300

    Autenticação com JWT

---
+++ src/main/java/com/majority/mynotebook/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.majority.mynotebook.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.majority.mynotebook.model.User;
+
+public interface UserRepository extends JpaRepository<User, Long>{
+
+	Optional<User> findByUsername(String username);
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,2 @@
+  
+INSERT INTO User(name, username, password) VALUES('Lucas Souza', 'lucas_souza', '$2a$10$8nNMBFm1RlFF6ImEeS4WUOcSKLR4HVDs0HvmVqoYQXOKPQSWcayFS');
